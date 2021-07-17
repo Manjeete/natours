19ffd7e76a97746b38af3490fb05266e7cb341f7
Manjeet Kumar <manjeetkr2017@gmail.com>
2021-07-17 07:19:20 +0530

    forgot password and reset password

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
@@ -40,7 +41,9 @@ const userSchema = new mongoose.Schema({
 
         }
     },
-    passwordChangedAt:Date
+    passwordChangedAt:Date,
+    passwordResetToken:String,
+    passwordResetExpires:Date
 });
 
 userSchema.pre('save',async function(next){
@@ -49,6 +52,14 @@ userSchema.pre('save',async function(next){
     this.password = await bcrypt.hash(this.password,12);
     this.passwordConfirm = undefined;
     next();
+});
+
+userSchema.pre('save',async function(next){
+    if(!this.isModified('password')) return next();
+
+    this.passwordChangedAt = Date.now()-1000;
+    next();
+
 });
 
 userSchema.methods.correctPassword = async function(candidatePassword,userPassword){
@@ -61,6 +72,15 @@ userSchema.methods.changedPasswordAfter = function(JWTTimestamp){
         return JWTTimestamp<changedTimestamp;
     }
     return false;
+};
+
+userSchema.methods.createPasswordResetToken = function(){
+    const resetToken = crypto.randomBytes(32).toString('hex');
+
+    this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+    this.passwordResetExpires = Date.now()+10*60*1000;
+    
+    return resetToken;
 };
 
 const User = mongoose.model('User',userSchema);
