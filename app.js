3d71fa877a79d210086cfbc3c53cccfadafc7f28
Manjeet Kumar <manjeetkr2017@gmail.com>
2021-07-17 22:16:06 +0530

    modeling review and created get and post request

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const hpp = require('hpp');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const AppError = require('./utils/appError');
 
@@ -21,9 +22,9 @@ const app = express();
 app.use(helmet());
 
 // Development logging
-if(process.env.NODE_ENV==='development'){
-    app.use(morgan('dev'));
-}
+// if(process.env.NODE_ENV==='development'){
+app.use(morgan('dev'));
+// }
 
 // Limit requests from same API
 const limiter = rateLimit({
@@ -38,10 +39,10 @@ app.use('/api',limiter);
 app.use(express.json({limit:'10kb'}));
 
 // Data sanitization against NoSql query injection
-app.use(mongoSanitize);
+app.use(mongoSanitize());
 
 //Data sanitization against XSS
-app.use(xss());
+// app.use(xss());
 
 //Prevent data polution
 app.use(hpp({
@@ -55,10 +56,10 @@ app.use(hpp({
     ]
 }));
 
-app.use((req,res,next) =>{
-    console.log('Hello from the Middleware 👏');
-    next();
-});
+// app.use((req,res,next) =>{
+//     console.log('Hello from the Middleware 👏');
+//     next();
+// });
 
 //test middlerware
 app.use((req,res,next) =>{
@@ -67,9 +68,9 @@ app.use((req,res,next) =>{
 });
 
 // Routes Middlewares
-
 app.use('/api/v1/tours',tourRouter);
 app.use('/api/v1/users/',userRouter);
+app.use('/api/v1/reviews',reviewRouter);
 
 // to handled unregister endpoint
 app.all('*',(req,res,next) =>{
