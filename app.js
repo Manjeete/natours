c0f6094112496e76083e47c893ac0b185daebbfa
Manjeet Kumar <manjeetkr2017@gmail.com>
2021-07-17 12:49:06 +0530

    auth completed

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanatize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -9,16 +15,52 @@ const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
-// middleware
-app.use(morgan('dev'));
+// global middleware
 
-app.use(express.json());
+//Set security HTTP headers
+app.use(helmet());
+
+// Development logging
+if(process.env.NODE_ENV==='development'){
+    app.use(morgan('dev'));
+}
+
+// Limit requests from same API
+const limiter = rateLimit({
+    max:100,
+    windowMs:60*60*1000,
+    message:'Too many requests from this IP, please try again in an hour!'
+});
+
+app.use('/api',limiter);
+
+// body parser, reading data from body into req.body
+app.use(express.json({limit:'10kb'}));
+
+// Data sanitization against NoSql query injection
+app.use(mongoSanitize);
+
+//Data sanitization against XSS
+app.use(xss());
+
+//Prevent data polution
+app.use(hpp({
+    whitelist:[
+        'duration',
+        'ratingsQuantity',
+        'ratingsAverage',
+        'maxGroupSize',
+        'difficulty',
+        'price'
+    ]
+}));
 
 app.use((req,res,next) =>{
     console.log('Hello from the Middleware 👏');
     next();
 });
 
+//test middlerware
 app.use((req,res,next) =>{
     req.requestTime = new Date().toISOString();
     next();
