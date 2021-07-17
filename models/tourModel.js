a452f92fb879a5afa946e213905a9846aaa92dc5
Manjeet Kumar <manjeetkr2017@gmail.com>
2021-07-17 15:51:57 +0530

    import tour data and modeling locations

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 
+const User = require('./userModel');
+
 const tourSchema = new mongoose.Schema({
     name:{
         type:String,
@@ -8,7 +10,7 @@ const tourSchema = new mongoose.Schema({
         unique:true,
         trim:true,
         maxlength:[40,'A tour name must have less or equal then 40 characters'],
-        maxlength:[10,'A tour name must have more or equal then 10 characters']
+        minlength:[10,'A tour name must have more or equal then 10 characters']
     },
     slug:String,
     duration:{
@@ -62,9 +64,32 @@ const tourSchema = new mongoose.Schema({
     secretTour:{
         type:Boolean
     },
-    startDates:[Date]
-
-
+    startDates:[Date],
+    startLocation:{
+        //GeoJSON
+        type:{
+            type:String,
+            default:'Point',
+            enum:['Point']
+        },
+        coordinates:[Number],
+        address:String,
+        description:String
+    },
+    locations:[
+        {
+            type:{
+                type:String,
+                default:'Point',
+                enum:['Point']
+            },
+            coordinates:[Number],
+            address:String,
+            description:String,
+            day:Number
+        }
+    ],
+    guides:Array
 },
 {
     toJSON:{virtuals:true},
@@ -80,6 +105,12 @@ tourSchema.pre('save',function(next){
     this.slug = slugify(this.name,{lower:true}); 
     next(); 
 });
+
+// tourSchema.pre('save',async function(next){
+//     const guidesPromises = this.guides.map(async id => await User.findById(id));
+//     this.guides = await Promise.all(guidesPromises);
+//     next();
+// });
 
 // tourSchema.post('save',function(doc,next){
 //     console.log(doc);
