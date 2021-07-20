5f6d451f3b8f2ca539742126574e77e8549f5d79
Manjeet Kumar <manjeetkr2017@gmail.com>
2021-07-20 16:21:32 +0530

    welcome email using mailtrap

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const jwt = require('jsonwebtoken');
 const AppError = require('../utils/appError');
-const sendEmail = require('../utils/email');
+const Email = require('../utils/email');
 
 
 const signToken = id =>{
@@ -42,6 +42,10 @@ exports.signup = catchAsync(async(req,res,next) =>{
         password:req.body.password,
         passwordConfirm:req.body.passwordConfirm 
     });
+
+    const url = `${req.protocol}://${req.get('host')}/updateMe`
+    console.log(url);
+    await new Email(newUser,url).sendWelcome();
     createSendToken(newUser,201,res);
     
 });
@@ -117,15 +121,15 @@ exports.forgotPassword = catchAsync(async (req,res,next)=>{
     const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to:${resetUrl}.\nIf you didn't 
     forget your password,please ignore this email.`;
     try{
-        await sendEmail({
-            email:user.email,
-            subject:'Your password reset token(valid for 10min)',
-            message
-        });
-        res.status(200).json({
-            status:'success',
-            message:'Token sent to email!'
-        });
+        // await sendEmail({
+        //     email:user.email,
+        //     subject:'Your password reset token(valid for 10min)',
+        //     message
+        // });
+        // res.status(200).json({
+        //     status:'success',
+        //     message:'Token sent to email!'
+        // });
     }catch(err){
         user.passwordResetToken=undefined;
         user.passwordResetExpires=undefined;
--- utils/email.js
+++ utils/email.js
@@ -1,27 +1,54 @@
 const nodemailer = require('nodemailer');
-const { options } = require('../routes/tourRoutes');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async options =>{
-    // 1) create a transporter
-    const transporter = nodemailer.createTransport({
-        host:process.env.EMAIL_HOST,
-        port:process.env.EMAIL_PORT,
-        auth:{
-            user:process.env.EMAIL_USERNAME,
-            pass:process.env.EMAIL_PASSWORD
-        }
-    });
-
-    // 2) Define the email options
-    const mailOptions = {
-        from:'Manjeet <manjeet@gmail.com>',
-        to:options.email,
-        subject:options.subject,
-        text:options.message
+module.exports = class Email{
+    constructor(user,url){
+        this.to = user.email;
+        this.firstName = user.name.split(' ')[0];
+        this.url = url;
+        this.from = `Manjeet Kumar <${process.env.EMAIL_FROM}>`;
     }
 
-    // 3) Actually send the email
-    await transporter.sendMail(mailOptions);
+    newTransport(){
+        if(process.env.NODE_ENV==='production'){
+            //sendgrid
+            return 1;
+        }
+
+        return nodemailer.createTransport({
+            host:process.env.EMAIL_HOST,
+            port:process.env.EMAIL_PORT,
+            auth:{
+                user:process.env.EMAIL_USERNAME,
+                pass:process.env.EMAIL_PASSWORD
+            }
+        });
+    }
+
+    async send(template,subject){
+        // 1) Render Html template on a pug template
+        const html = pug.renderFile(`./views/emails/${template}.pug`,
+        {
+            firstName:this.firstName,
+            url:this.url,
+            subject
+        });
+
+        // 2) Define email options
+        const mailOptions = {
+            from:this.from,
+            to:this.to,
+            subject:subject,
+            html:html,
+            // text:htmlToText.convert(html)
+        }
+        // 3) create a transport and send email
+        await this.newTransport().sendMail(mailOptions);
+    };
+
+    async sendWelcome(){
+        await this.send('welcome','Welcome to the natours family');
+    }
 };
 
-module.exports = sendEmail;
