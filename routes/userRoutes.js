077abef412c74877151d260a3a902640755f6667
Manjeet Kumar <manjeetkr2017@gmail.com>
2021-07-20 11:44:46 +0530

    image processed

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,7 +14,7 @@ router.patch('/updateMyPassword',authController.protect,authController.updatePas
 router.use(authController.protect);
 
 router.get('/me',userController.getMe,userController.getUser);
-router.patch('/updateMe',userController.uploadUserPhoto,userController.updateMe);
+router.patch('/updateMe',userController.uploadUserPhoto,userController.resizeUserPhoto,userController.updateMe);
 router.delete('/deleteMe',userController.deleteMe);
 
 router.use(authController.restrictTo('admin'));
