f759b18d03366461c0707922750630e73840616a
Manjeet Kumar <manjeetkr2017@gmail.com>
2021-07-21 17:56:59 +0530

    modified config variable

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = require('./app');
 dotenv.config({path:'./config.env'});
 
 //db
-mongoose.connect(process.env.DATABASE_CLOUD,{useNewUrlParser:true,useCreateIndex:true,useFindAndModify:false,useUnifiedTopology: true })
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@seoblog.hzlj5.mongodb.net/natours?retryWrites=true&w=majority`,{useNewUrlParser:true,useCreateIndex:true,useFindAndModify:false,useUnifiedTopology: true })
 .then(() => console.log('DB Connected...'))
 
 
